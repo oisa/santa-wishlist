344201d7fb3c215a07caee6bc269a2d459697b47
oisa <imucup@gmail.com>
2021-05-28 14:15:39 +1000

    Adds refined styling and hooks to capture state of all three data touchpoints.

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+
+// Components
+import Wishlist from './Wishlist';
+
+// Styling
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Santa's Wishlist!</h1>
+      </header>
+      <Wishlist />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Wishlist.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const Wishlist = () => {
+
+  const [wish, setWish] = useState({
+    name: '',
+    wish: '',
+    priority: '1'
+  });
+
+  const handleChange = (event) => {
+    setWish({
+      ...wish,
+      [event.target.name]: event.target.value,
+      [event.target.wish]: event.target.value,
+      [event.target.priority]: event.target.value,
+     });
+  }
+
+  const handleSubmit = () => {
+
+  }
+
+  return (
+    <div>
+      <h2>Tell Santa what you want for Christmas!</h2>
+
+    <form>
+      <label>You name:
+        <input id="name" name="name" type="text" placeholder="Sven" required value={ wish.name } onChange={ handleChange }></input>
+      </label>
+      <label>What do you want?
+        <textarea id="wish" name="wish" type="textarea" placeholder="10 hippos and a side of bacon for me please Santa!" rows="10" cols="50" required value={ wish.wish } onChange={ handleChange }></textarea>
+      </label>
+      <label>Priority:
+        <select id="priority" name="priority" value={ wish.priority } onChange={ handleChange }>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </label>
+      <button onSubmit={ handleSubmit }>Send to Santa!</button>
+    </form>
+    </div>
+  )
+}
+
+export default Wishlist;
